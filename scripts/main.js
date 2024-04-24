53ecd80d51c1c409c3d67bb6ce8871c269817f36
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-04-24 21:56:27 +0000

    task 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,30 @@
-console.log()
-
 // Task #1: Print the first 10 square numbers (1*1 = 1, 2*2 = 4, etc.)
+console.log("Task 1");
 
-for (let i = 0; i >= 10; i++) {
-    let square = i*i;
-    println(i + "squared is" + square);
+for (let i = 0; i <= 10; i++) {
+    console.log(i*i)
 }
 
 // Task #2: Count down from 5 to 1 then say "Blastoff!"
-
+console.log("Task 2");
+for (let i = 5; i > 0; i--) {
+    console.log("blast off!");
+}
 
 // Task #3: Print the even numbers from 1 to 50 (including 50)
+console.log("Task 3");
+ //for(let i = 1; 1 <= 50; i++) {
+ //}
+
 // Task #4: Print the sum of 1 through 100.
-let sum = 0;
+console.log("Task 4");
+
+//let sum = 0;
 
 
 // Task #5: Print the first 10 factorials. The factorial of a number n is n multiplied by each integer less than it, down to 1, written as n! 
     //1! = 1
     //2! = 2 * 1 = 2
     //3! = 3 * 2 * 1 = 6 etc.
+console.log("Task 5");
     let iFactorial = 1;
