02ed06fd3b81204ea715cae4f438a83c72c435b7
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-05-06 15:38:34 +0000

    rainbow js yay

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,1 +1,11 @@
-colorArr = ["darkred", "darkorange", "gold", "darkseagreen", "cadetblue", "darkslateblue", "darkviolet"]
+let colorArr = ["darkred", "darkorange", "gold", "darkseagreen", "cadetblue", "darkslateblue", "darkviolet"]
+let cat = document.createElement("ul");
+
+for (let i = 0; i < colorArr.length; i++){
+    let li = document.createElement("li");
+    li.innerText = colorArr[i];
+    li.style.color = colorArr[i];
+    cat.appendChild(li);
+}
+
+document.body.appendChild(cat);
