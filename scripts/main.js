465da13a307c64195e9c923a0e8301f7a6526f0f
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-05-15 21:05:46 +0000

    cursor ice cream

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+console.log("yay")
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+
+window.addEventListener("mousemove", function(e){
+    const positionX = aohfkahf;
+    const positionY = ajshdkahsdkajd;
+
+});
