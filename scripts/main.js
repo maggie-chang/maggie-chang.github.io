f5bf201a4c7596d85dafacce6f83333f40f24097
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-05-17 00:33:33 +0000

    dot cursor

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
 console.log("yay")
 
-const cursorDot = document.querySelector("[data-cursor-dot]");
+//const cursorDot = document.querySelector("[data-cursor-circle]");
 
-window.addEventListener("mousemove", function(e){
-    const positionX = aohfkahf;
-    const positionY = ajshdkahsdkajd;
+//window.addEventListener("mousemove", function(e){
+    //const positionX = aohfkahf;
+    //const positionY = ajshdkahsdkajd;
+//});
 
-});
+//document.addEventListener('mousemove', function(e) {
+    //let body = document.querySelector('body');
+    //let circle = document.getElementById('circle');
+    //let left = e.offsetX;
+    //let top = e.offsetY;
+    //circle.style.left = left + 'px';
+    //circle.style.top = top + 'px';
+  //});
