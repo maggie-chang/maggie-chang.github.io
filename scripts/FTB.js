5b8656c9488b05f301355630a8c030eb761d666f
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-05-24 16:12:51 +0000

    ftb scripts ig

--- scripts/FTB.js
+++ scripts/FTB.js
@@ -1,11 +1,9 @@
 const buttonEl = document.getElementsByClassName("btn");
 document.body.appendChild(buttonEl);
 
-buttonEl.addEventListener("click", fuction(e)); {
-    <a href="/index.html"></a>;
-
-};
-
-function usingThis() {
-    
-}
+buttonEl.addEventListener("click", fuction(){
+    if(("buttonEl").clicked == true)
+        {
+        <a href="/index.html"></a>;
+        }
+});
