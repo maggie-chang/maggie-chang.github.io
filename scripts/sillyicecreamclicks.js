b2022d81791ed06dbf27cea6f352f4e58c8dd7dc
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-06-03 16:11:13 +0000

    sillyicecreamclicks and some css

--- scripts/sillyicecreamclicks.js
+++ scripts/sillyicecreamclicks.js
@@ -6,7 +6,7 @@ sillyBody.addEventListener("click", function(){
     numClicks++;
     console.log(numClicks)
     clickedtimes.innerText = numClicks;
-    if (numClicks == 10000){
+    if (numClicks == 204){
         clickedtimes.innerText = "too many clicks";
         window.location.assign("/");
     }
