2a0599ca7a8df699487dc2f5745d9ac652bf6bac
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-06-07 17:10:59 +0000

    mi web final yyayayay

--- scripts/FTB.js
+++ scripts/FTB.js
@@ -6,3 +6,9 @@ buttonEl.style.top = Math.random() * 700 + "px";
 buttonEl.addEventListener("click", function(){
     window.location.assign("/");
 });
+
+let colorArr = ["darkred", "darkorange", "gold", "darkseagreen", "cadetblue", "darkslateblue", "darkviolet"]
+const bgColor = document.querySelector("body");
+
+document.body.style.background = colorArr[Math.floor(Math.random()*colorArr.length)];
+document.body.appendChild(bgColor);
